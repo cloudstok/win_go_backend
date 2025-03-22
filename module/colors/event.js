e13f6e6ad1b865de4dc0fc9aba40b6896d1e156f
Prashantg30 <prashant.gupta@cloudstok.com>
2025-03-22 18:18:57 +0000

    RTP logic updated

--- module/colors/event.js
+++ module/colors/event.js
@@ -8,62 +8,11 @@ const initColor = async (io) => {
     logger.info("lobby started");
     const delays = [30, 60, 180, 300];
     delays.forEach((delay, index) => {
-        const results = getResultNumber();
-        initLobby(io, delay, index + 101, results);
+        initLobby(io, delay, index + 101);
     })
 };
 
-function getSingleResult(array, index){
-    let value = 0;
-    switch(index){
-        case 101: value = array[0];
-        case 102: value = array[1];
-        case 103: value = array[2];
-        case 104: value = array[3];
-        default: value;
-    };
-    return value;
-}
-
-function getResultNumber() {
-    let result = [];
-    let odds = [];
-    let evens = [];
-
-    let oddCount =  Math.floor(Math.random() * 5);
-    let evenCount = 4 - oddCount;
-
-    while (odds.length < oddCount || evens.length < evenCount) {
-        let randomNumber = Math.floor(Math.random() * 10);
-        if (randomNumber % 2 === 0 && evens.length < evenCount) {
-            if (!evens.includes(randomNumber)) {
-                evens.push(randomNumber);
-            }
-        } else if (randomNumber % 2 !== 0 && odds.length < oddCount) {
-            if (!odds.includes(randomNumber)) {
-                odds.push(randomNumber);
-            }
-        }
-    }
-
-    result = odds.concat(evens);
-    result = shuffleArray(result);
-    return result;
-}
-
-function shuffleArray(array) {
-    const shuffledArray = [...array];
-  
-    for (let i = shuffledArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1)); // Generate a 
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-    }
-  
-    return shuffledArray;
-  }
-  
-
-const initLobby = async (io, delay, lobbyNumber, results) => {
+const initLobby = async (io, delay, lobbyNumber) => {
     const date = new Date();
     const year = date.getFullYear();
     const month = date.getMonth() + 1 + '';
@@ -76,7 +25,7 @@ const initLobby = async (io, delay, lobbyNumber, results) => {
 
     setCurrentLobby(lobbyNumber, recurLobbyData);
     let start_delay = delay;
-    const result = getSingleResult(results, lobbyNumber);
+    const result = Math.floor(Math.random() * 10);
     const end_delay = 2;
 
     for (let x = start_delay; x > 0; x--) {
@@ -103,7 +52,7 @@ const initLobby = async (io, delay, lobbyNumber, results) => {
     io.emit("history", JSON.stringify({ roomId: history.roomId, result }));
     logger.info(JSON.stringify(history));
     await insertLobbies(history);
-    return initLobby(io, delay, lobbyNumber, getResultNumber());
+    return initLobby(io, delay, lobbyNumber);
 }
 
 module.exports = { initColor }
